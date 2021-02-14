b1c3705a34354988757454bc5bb42aecfd0a66fe
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2021-02-14 07:00:59 -0300

    feat: add hello command

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 require('dotenv').config();
 
+const { Client } = require('discord.js');
+const client = new Client();
+
+const PREFIX = process.env.BOT_PREFIX;
+
+client.on('ready', () => {
+  console.log(`${client.user.username} shouts FUS RO DAH`);
+});
+
+client.on('message', (message) => {
+  if(message.author.bot) return;
+
+  if(message.content.startsWith(PREFIX)) {
+    const [command, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+
+      if(command === 'hello'){
+        message.channel.send('FUS RO DAH');
+      }
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
